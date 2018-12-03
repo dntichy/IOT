fd37cb38db7cf8693edc5a18ba82f3bc351cc3ec
dtichy <dntich@gmail.com>
2018-12-03 09:56:27 +0100

    first commit

---
+++ IoT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tinkerforge;
+
+namespace IoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component cp = new Component();
+            cp.Connect();
+
+            Console.WriteLine("Temperature: " + cp.ReadTemperature() / 100.0 + " °C");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+            cp.Disconnect();
+        }
+    }
+}
